00a25f4bb71f8421c9bebbdb0bb405adf94b0b51
Massagli <adrielmassagli@gmail.com>
2024-05-23 14:50:18 -0300

    Add JFrames

---
+++ EducativaProject/src/main/java/com/mycompany/educativaproject/EducativaProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.educativaproject;
+
+/**
+ *
+ * @author adrie
+ */
+public class EducativaProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
