4add8557668056af9b34cf15f8dbca7feb70c846
Massagli <adrielmassagli@gmail.com>
2024-05-24 20:37:52 -0300

    Create Last Frame

--- EducativaProject/src/main/java/com/mycompany/educativaproject/EducativaProject.java
+++ EducativaProject/src/main/java/com/mycompany/educativaproject/EducativaProject.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.educativaproject;
 
-/**
- *
- * @author adrie
- */
 public class EducativaProject {
 
     public static void main(String[] args) {
